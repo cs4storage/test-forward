cec17ac5d4dbe7212cb3193db54819024e88ac92
helloworld <helloworld@heroku.com>
2022-07-29 22:30:56 +0800

    add forward

--- netlify/edge-functions/tmp/[page].js
+++ netlify/edge-functions/tmp/[page].js
@@ -1,30 +1,30 @@
 // A global page template for all of our HTML views
 // You know, for consistency.
-import layout from "../../components/layout.js";
+import layout from "../../../components/layout.js";
 
 // A bit convoluted, but necessary for the moment
 // until we can import the page templates we need
 // dynamically from withing the exported function
-import pageHome from "../../pages/home/index.js";
-import pageHello from "../../pages/hello/index.js";
-import pageTransform from "../../pages/transform/index.js";
-import pageInclude from "../../pages/include/index.js";
-import pageRewrite from "../../pages/rewrite/index.js";
-import pageGeolocation from "../../pages/geolocation/index.js";
-import pageJson from "../../pages/json/index.js";
-import pageSetRequestHeader from "../../pages/set-request-header/index.js";
-import pageSetResponseHeader from "../../pages/set-response-header/index.js";
-import pageCookiesSet from "../../pages/cookies-set/index.js";
-import pageCookiesRead from "../../pages/cookies-read/index.js";
-import pageCookiesDelete from "../../pages/cookies-delete/index.js";
-import pageABTest from "../../pages/abtest/index.js";
-import pageImage from "../../pages/image/index.js";
-import pageLog from "../../pages/log/index.js";
-import pageCountryBlock from "../../pages/country-block/index.js";
-import pageLocalizedContent from "../../pages/localized-content/index.js";
-import pageProxyRequests from "../../pages/proxy-requests/index.js";
-import pageEnvironment from "../../pages/environment/index.js";
-import pageUncaughtExceptions from "../../pages/uncaught-exceptions/index.js";
+import pageHome from "../../../pages/home/index.js";
+import pageHello from "../../../pages/hello/index.js";
+import pageTransform from "../../../pages/transform/index.js";
+import pageInclude from "../../../pages/include/index.js";
+import pageRewrite from "../../../pages/rewrite/index.js";
+import pageGeolocation from "../../../pages/geolocation/index.js";
+import pageJson from "../../../pages/json/index.js";
+import pageSetRequestHeader from "../../../pages/set-request-header/index.js";
+import pageSetResponseHeader from "../../../pages/set-response-header/index.js";
+import pageCookiesSet from "../../../pages/cookies-set/index.js";
+import pageCookiesRead from "../../../pages/cookies-read/index.js";
+import pageCookiesDelete from "../../../pages/cookies-delete/index.js";
+import pageABTest from "../../../pages/abtest/index.js";
+import pageImage from "../../../pages/image/index.js";
+import pageLog from "../../../pages/log/index.js";
+import pageCountryBlock from "../../../pages/country-block/index.js";
+import pageLocalizedContent from "../../../pages/localized-content/index.js";
+import pageProxyRequests from "../../../pages/proxy-requests/index.js";
+import pageEnvironment from "../../../pages/environment/index.js";
+import pageUncaughtExceptions from "../../../pages/uncaught-exceptions/index.js";
 
 // The keys here correspond to the path in the request to `/example/PATH`
 const pages = {
